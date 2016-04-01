ab51997bb7095ab60d4ec8c8f292ab38c3c5291a
mike_neck <jkrt3333@gmail.com>
2016-04-01 13:01:27 +0900

    lesson1 task11 done

--- education/lesson1/task11/src/Task.kt
+++ education/lesson1/task11/src/Task.kt
@@ -2,6 +2,8 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(l: Int, r: Int): Int = r - l
+    })
     return arrayList
 }
