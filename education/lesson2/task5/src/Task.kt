efbf4e33cdde3172568adac7d0fc0223c6213cd9
mike_neck <jkrt3333@gmail.com>
2016-04-01 14:13:51 +0900

    lesson2 task5 done

--- education/lesson2/task5/src/Task.kt
+++ education/lesson2/task5/src/Task.kt
@@ -4,12 +4,18 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+data class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeInterval: RepeatedTimeInterval): MyDate = addTimeIntervals(timeInterval.timeInterval, timeInterval.number)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
