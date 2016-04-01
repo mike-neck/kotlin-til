982328bcfdacd770ab52b7e4cbc7fbb0478c652c
mike_neck <jkrt3333@gmail.com>
2016-04-01 13:59:10 +0900

    lesson2 task4 done

--- education/lesson2/task4/src/Task.kt
+++ education/lesson2/task4/src/Task.kt
@@ -1,4 +1,19 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+}
+
+class DateIterator(val dateRange: DateRange): Iterator<MyDate> {
+    var current = dateRange.start
+    override fun hasNext(): Boolean {
+        return current < dateRange.endInclusive
+    }
+
+    override fun next(): MyDate {
+        val result = current
+        current = result.nextDay()
+        return result
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
