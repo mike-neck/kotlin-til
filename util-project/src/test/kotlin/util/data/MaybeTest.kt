722992660e01e5d889113f60be39cc1eb2b9b92a
mike_neck <jkrt3333@gmail.com>
2016-08-25 09:34:19 +0900

    add monad 3rd law test to maybe

--- util-project/src/test/kotlin/util/data/MaybeTest.kt
+++ util-project/src/test/kotlin/util/data/MaybeTest.kt
@@ -16,9 +16,12 @@
 package util.data
 
 import org.junit.Test
+import util.data.MaybeInstance.bind
 import util.data.MaybeInstance.fn
 import util.data.MaybeInstance.map
 import util.data.MaybeInstance.pure
+import util.data.MaybeOf.Just
+import util.data.MaybeOf.None
 import util.plus
 import util.shouldBe
 import util.unit
@@ -63,7 +66,7 @@ class MaybeTest {
     @Test fun monad1stLaw() =
             listOf(0, 1, -1, 1000000, -1000000).forEach(satisfyLeftIdentity)
 
-    val positive: (Int) -> Maybe<Int> = { if (it >= 0) MaybeOf.Just(it) else MaybeOf.None() }
+    val positive: (Int) -> Maybe<Int> = { if (it >= 0) Just(it) else None() }
 
     val satisfyLeftIdentity: (Int) -> Unit = { returnThenBind(it) shouldBe simplyApply(it) }
 
@@ -79,13 +82,36 @@ class MaybeTest {
      * </code></pre>
      */
     @Test fun monad2ndLaw() =
-            listOf<Maybe<Maybe<Int>>>(MaybeOf.Just(MaybeOf.Just(1)), MaybeOf.Just(MaybeOf.None()), MaybeOf.None())
+            listOf<Maybe<Maybe<Int>>>(Just(Just(1)), Just(None()), None())
                     .forEach(satisfyRightIdentity)
 
     val bindReturn: (Maybe<Maybe<Int>>) -> Maybe<Maybe<Int>> =
             fn<Maybe<Int>, Maybe<Int>>().let { it.bind / it.pure }
 
     val satisfyRightIdentity: (Maybe<Maybe<Int>>) -> Unit = { bindReturn(it) shouldBe it }
+
+    /**
+     * A test that [Maybe] satisfies monad 3rd law(Associativity).
+     *
+     * <pre><code>
+     *     (m >>= f) >>= g = m >>= (\x -> f x >>= g)
+     * </code></pre>
+     */
+    @Test fun monad3rdLaw() =
+            listOf<Maybe<String>>(Just("MaybeOf"), Just(""), None(), Just("maybe"), Just("MAYBE"))
+                    .forEach(satisfyAssociativity)
+
+    val f3: (String) -> Maybe<List<Char>> = { if (it.length > 0) Just(it.toCharArray().toList()) else None() }
+
+    val g3: (List<Char>) -> Maybe<String> = { it.filter(Char::isUpperCase)
+            .let { if (it.size == 0) None() else Just(it.joinToString("")) } }
+
+    val satisfyAssociativity: (Maybe<String>) -> Unit = { bindThenBind(it) shouldBe bindBinding(it) }
+
+    val bindThenBind: (Maybe<String>) -> Maybe<String> =
+            (fn<String, List<Char>>().bind / f3) + (fn<List<Char>, String>().bind / g3)
+
+    val bindBinding: (Maybe<String>) -> Maybe<String> = fn<String, String>().bind / {x -> bind(f3(x), g3) }
 }
 
 infix operator fun <P, Q, R, F:(P) -> ((Q) -> R)> F.div(q: Q): (P) -> R = { p: P -> this(p)(q) }
